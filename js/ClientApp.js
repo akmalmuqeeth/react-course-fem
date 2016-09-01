2fb8db67a0cb422e63caacd1fbd546e6403d1c10
akmal <akmal@brighter.com>
2016-08-31 18:10:38 -0700

    initial basic setup with react using webpack and standard

---
+++ js/ClientApp.js
@@ -0,0 +1,21 @@
+/* global React ReactDOM */
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var div = React.DOM.div;
+
+var MyTitle = require('./MyTitle');
+var MyTitleFactory = React.createFactory(MyTitle);
+var ce = React.createElement;
+
+var myFirstComponent = (
+  div(null,
+    MyTitleFactory({title: 'Props are great', color: 'purple'}),
+    React.createElement(MyTitle, {title: 'User props', color: 'mediumaquamarine'}),
+    ce(MyTitle, {title: 'The bestttt', color: 'peru'})
+
+  )
+);
+
+ReactDOM.render(myFirstComponent, document.getElementById('app'));
