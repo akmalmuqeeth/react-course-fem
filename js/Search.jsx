cd73dd977f69b87c569896c5ddcd01abfca2fbed
akmal <akmal@brighter.com>
2016-09-07 14:15:23 -0700

    convert the stateless component to a stateful component
    satte is anot added yet

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,16 +3,25 @@ const data = require('../public/data')
 
 const ShowCard = require('./ShowCard')
 
-const Search = () => (
-  <div className="container">
-    <div className="shows">
-      {
-        data.shows.map((show) => (
-          <ShowCard {...show} key={show.imdbID} />
-        ))
-      }
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  render () {
+    return (
+      <div className="container">
+        <header className="header">
+          <h1 className="brand">svideo</h1>
+          <input type="text" placeholder="Search" className="search-input" />
+        </header>
+
+        <div className="shows">
+          {
+            data.shows.map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
