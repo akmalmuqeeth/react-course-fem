e5a43642857db45c83974def29785f1d371d766e
akmal <akmal@brighter.com>
2016-09-09 17:13:44 -0700

    add details route which has route params

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -3,6 +3,7 @@ const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Layout = require('./Layout')
 const Search = require('./Search')
+const Details = require('./Details')
 
 const {Router, Route, IndexRoute, hashHistory} = require('react-router')
 
@@ -11,6 +12,7 @@ const App = () => (
     <Route path='/' component={Layout} >
       <IndexRoute component={Landing} />
       <Route path='/search' component={Search} />
+      <Route path='/details/:id' component={Details} />
     </Route>
   </Router>
 )
