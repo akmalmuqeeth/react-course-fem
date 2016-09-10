4e5023f79a25cdf05f63908ce461e17ac7ececaf
akmal <akmal@brighter.com>
2016-09-09 21:13:57 -0700

    pass the shows array as props from the client app

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -4,14 +4,14 @@ const Landing = require('./Landing')
 const Layout = require('./Layout')
 const Search = require('./Search')
 const Details = require('./Details')
-
 const {Router, Route, IndexRoute, hashHistory} = require('react-router')
+const {shows} = require('../public/data')
 
 const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Layout} >
       <IndexRoute component={Landing} />
-      <Route path='/search' component={Search} />
+      <Route path='/search' component={Search} shows={shows} />
       <Route path='/details/:id' component={Details} />
     </Route>
   </Router>
