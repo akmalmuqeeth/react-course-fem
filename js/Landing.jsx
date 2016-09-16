c339efb743de253c2707730c6a1a32f1a0b46f40
akmal <akmal@brighter.com>
2016-09-15 18:04:17 -0700

    my fix to link the landing page with the search page

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,12 +1,25 @@
 const React = require('react')
 const {Link} = require('react-router')
+const {connector} = require('./Store')
+const { func, string } = React.PropTypes
 
-const Landing = () => (
-  <div className='home-info'>
-    <h1 className='title'>svideo</h1>
-    <input className='search' type='text' placeholder='Search' />
-    <Link to="/search" className='browse-all'> or Browse Here</Link>
-  </div>
-)
+const Landing = (props) => {
+  function handleSearchTermEvent (event) {
+    props.setSearchTerm(event.target.value)
+  }
 
-module.exports = Landing
+  return (
+    <div className='home-info'>
+      <h1 className='title'>svideo</h1>
+      <input className='search' type='text' placeholder='Search' value={props.searchTerm} onChange={handleSearchTermEvent} />
+      <Link to="/search" className='browse-all'> or Browse Here</Link>
+    </div>
+  )
+}
+
+Landing.propTypes = {
+  searchTerm: string,
+  setSearchTerm: func
+}
+
+module.exports = connector(Landing)
