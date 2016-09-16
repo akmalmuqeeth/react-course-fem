4e70e8bc5e64d3ce90bbd7e149966b16e70a4140
akmal <akmal@brighter.com>
2016-09-15 18:12:32 -0700

    convert Landing to a stateful component

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,21 +1,36 @@
 const React = require('react')
-const {Link} = require('react-router')
-const {connector} = require('./Store')
-const { func, string } = React.PropTypes
+const { hashHistory } = require('react-router')
+const { Link } = require('react-router')
+const { connector } = require('./Store')
 
-const Landing = (props) => {
-  function handleSearchTermEvent (event) {
-    props.setSearchTerm(event.target.value)
+class Landing extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+    this.gotoSearch = this.gotoSearch.bind(this)
   }
-
-  return (
-    <div className='home-info'>
-      <h1 className='title'>svideo</h1>
-      <input className='search' type='text' placeholder='Search' value={props.searchTerm} onChange={handleSearchTermEvent} />
-      <Link to="/search" className='browse-all'> or Browse Here</Link>
-    </div>
-  )
+  handleSearchTermEvent (event) {
+    this.props.setSearchTerm(event.target.value)
+  }
+  gotoSearch (event) {
+    hashHistory.push('search')
+    event.preventDefault()
+  }
+  render () {
+    return (
+      <div className='home-info'>
+        <h1 className='title'>svideo</h1>
+        <form onSubmit={this.gotoSearch}>
+          <input value={this.props.searchTerm} onChange={this.handleSearchTermEvent} className='search' type='text' placeholder='Search' />
+        </form>
+        <Link to='/search' className='browse-all'> or Browse All</Link>
+      </div>
+    )
+  }
 }
+
+const { func, string } = React.PropTypes
 
 Landing.propTypes = {
   searchTerm: string,
