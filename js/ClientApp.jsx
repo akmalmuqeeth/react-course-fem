d87b3998da1715fa89a491bd157760d11bca0d7f
akmal <akmal@brighter.com>
2016-09-19 13:40:14 -0700

    server side rendering and web chunking

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,29 +1,56 @@
 const React = require('react')
-const Landing = require('./Landing')
 const Layout = require('./Layout')
-const Search = require('./Search')
-const Details = require('./Details')
-const {Router, Route, IndexRoute, hashHistory} = require('react-router')
-const {store} = require('./Store')
-const {Provider} = require('react-redux')
+const { Router, browserHistory } = require('react-router')
+const { store } = require('./Store')
+const { Provider } = require('react-redux')
+
+if (typeof module !== 'undefined' && module.require) {
+  if (typeof require.ensure === 'undefined') {
+    require.ensure = require('node-ensure') // shim for node.js
+  }
+}
+
+const rootRoute = {
+  component: Layout,
+  path: '/',
+  indexRoute: {
+    getComponent (location, cb) {
+      require.ensure([], () => {
+        cb(null, require('./Landing'))
+      })
+    }
+  },
+  childRoutes: [
+    {
+      path: 'search',
+      getComponent (location, cb) {
+        require.ensure([], () => {
+          cb(null, require('./Search'))
+        })
+      }
+    },
+    {
+      path: 'details/:id',
+      getComponent (location, cb) {
+        require.ensure([], () => {
+          cb(null, require('./Details'))
+        })
+      }
+    }
+  ]
+}
 
 const App = React.createClass({
   render () {
     return (
-      // Provider makes our redux store available throughout the app
-      // anywhere we use connector its going to find the redux store and plug it in
       <Provider store={store}>
-        <Router history={hashHistory}>
-          <Route path='/' component={Layout} >
-            <IndexRoute component={Landing} />
-            <Route path='/search' component={Search} />
-            <Route path='/details/:id' component={Details} />
-          </Route>
-        </Router>
+        <Router history={browserHistory} routes={rootRoute} />
       </Provider>
-
     )
   }
 })
+
+App.Routes = rootRoute
+App.History = browserHistory
 
 module.exports = App
